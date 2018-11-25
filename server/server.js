08cbd0127fd0f8ff8116c35e828faa47a7813af0
SE <sk@SEs-MacBook-Pro.local>
2018-11-25 20:48:25 +0100

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -21,6 +21,13 @@ app.post('/todos', (req, res) => {
 });
 
 //GET /todos/_id=192910
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  })
+});
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
