2d17045613dd62a575d7a2b801057c4c4e816b5d
SE <sk@SEs-MacBook-Pro.local>
2018-11-30 21:04:09 +0100

    minor editing of mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,13 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise; //setup to use promises
 //mongoose.connect('mongodb://localhost:27017/TodoApp');
 //mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
-mongoose.connect('mongodb://whomarketsme:getitDone_34?mmt@ds123224.mlab.com:23224/todo-app');
+
+let db = {
+  localhost: 'mongodb://localhost:27017/TodoApp',
+  mlab: 'mongodb://whomarketsme:getitDone_34?mmt@ds123224.mlab.com:23224/todo-app'
+};
+
+mongoose.connect(db.localhost || db.mlab);
 
 module.exports = {
   mongoose
