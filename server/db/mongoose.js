3f6cc536a3fd308cc8bdf2eec2c1ea86201faafc
SE <sk@SEs-MacBook-Pro.local>
2018-12-01 21:13:17 +0100

    Setup separate test and development environments

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,6 +4,4 @@ mongoose.Promise = global.Promise; //setup to use promises
 //mongoose.connect('mongodb://localhost:27017/TodoApp');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
-module.exports = {
-  mongoose
-};
+module.exports = {mongoose};
